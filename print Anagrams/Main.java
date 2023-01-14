eddc3fc9604c69168c17af72fb14267bbe041ae2
Nren <imnren07@gmail.com>
2023-01-14 11:52:57 +0530

    hello

--- print Anagrams/Main.java
+++ print Anagrams/Main.java
@@ -2,42 +2,39 @@
 // A Java program to print all anagrams together
 import java.util.*;
 
-	public class Main {
-	    static String sortStr(String s) {
-	    // convert string to arr
-	    char[] arr = s.toCharArray();
-	    // sort the array
-	    Arrays.sort(arr);
-	    // convert arr to string
-	    return new String(arr);
-	}
- 
+public class Main {
+    static String sortStr(String s) {
 
+        char[] arr = s.toCharArray();
+        // sort the array
+        Arrays.sort(arr);
+        // convert arr to string
+        return new String(arr);
+    }
     static void printAnagramsTogether(String strs[],
             int n) {
         Map<String, List<String>> hm = new TreeMap<>();
-	 
-	    for(int i = 0; i < n; i++) {
-	        String original = strs[i];
-	        String sorted = sortStr(original);
-	        if(hm.containsKey(sorted)) {
-	            List<String>oldlist = hm.get(sorted);
-	            oldlist.add(original);
-	            hm.put(sorted, oldlist);
-	        }
-	        else {
-	            List<String>newlist = new ArrayList<>();
-	            newlist.add(original);
-	            hm.put(sorted, newlist);
-	        }
-	    }
-	
-		for(String key:hm.keySet()){
-			List<String>ans=hm.get(key);
-			for(int i=0;i<ans.size();i++){
-				System.out.print(ans.get(i)+" ");
-			}
-		}
+
+        for (int i = 0; i < n; i++) {
+            String original = strs[i];
+            String sorted = sortStr(original);
+            if (hm.containsKey(sorted)) {
+                List<String> oldlist = hm.get(sorted);
+                oldlist.add(original);
+                hm.put(sorted, oldlist);
+            } else {
+                List<String> newlist = new ArrayList<>();
+                newlist.add(original);
+                hm.put(sorted, newlist);
+            }
+        }
+
+        for (String key : hm.keySet()) {
+            List<String> ans = hm.get(key);
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.print(ans.get(i) + " ");
+            }
+        }
     }
 
     // Driver program to test above functions
